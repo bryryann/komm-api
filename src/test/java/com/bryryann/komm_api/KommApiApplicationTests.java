78f6f96b8ade3539d798e37272cce92ef1211260
bryryann <186111776+bryryann@users.noreply.github.com>
2025-03-25 10:05:06 -0300

    Initialize Spring Boot project.

---
+++ src/test/java/com/bryryann/komm_api/KommApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryryann.komm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
